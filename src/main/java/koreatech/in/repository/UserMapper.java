a2ee784dfc28bc69fdadedae74119c5c38fefa6b
ChangyeonYoo <devwgod@gmail.com>
2021-04-18 22:30:07 +0900

    DB에서 발생하는 키 중복 에러 핸들링

--- src/main/java/koreatech/in/repository/UserMapper.java
+++ src/main/java/koreatech/in/repository/UserMapper.java
@@ -19,7 +19,7 @@ public interface UserMapper {
     @Insert("INSERT INTO koin.users (PORTAL_ACCOUNT, PASSWORD, NAME, NICKNAME, GENDER, IDENTITY, IS_GRADUATED, MAJOR, STUDENT_NUMBER, PHONE_NUMBER, AUTH_TOKEN, AUTH_EXPIRED_AT, IS_AUTHED, ANONYMOUS_NICKNAME, PROFILE_IMAGE_URL) " +
             "VALUES (#{portal_account}, #{password}, #{name}, #{nickname}, #{gender}, #{identity}, #{is_graduated}, #{major}, #{student_number}, #{phone_number}, #{auth_token}, #{auth_expired_at}, #{is_authed}, #{anonymous_nickname}, #{profile_image_url})")
     @SelectKey(statement = "SELECT LAST_INSERT_ID()", keyProperty = "id", before = false, resultType = int.class)
-    void createUser(User user);
+    void createUser(User user) throws Exception;
 
     @Delete("DELETE FROM koin.users WHERE ID = #{id}")
     void deleteUser(@Param("id") int id);
--- src/main/java/koreatech/in/service/UserServiceImpl.java
+++ src/main/java/koreatech/in/service/UserServiceImpl.java
@@ -27,6 +27,7 @@ import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import javax.inject.Inject;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -392,7 +393,13 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
         // 추후 메일 인증에 필요한 가입 정보를 디비에 업데이트
         if (selectUser == null) {
-            userMapper.createUser(user);
+            try {
+                userMapper.createUser(user);
+            } catch (SQLException sqlException){
+                if(sqlException.getErrorCode() == 1062)
+                    throw new ConflictException(new ErrorMessage("invalid authenticate", 0));
+            }
+
         } else {
             user.setId(selectUser.getId());
             userMapper.updateUser(user);
