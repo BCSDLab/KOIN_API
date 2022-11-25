3e39e59a625ac8e9fb1bf07e81519f9c6488e89d
Kim-Juwon <damiano102777@naver.com>
2022-11-25 16:01:30 +0900

    refactor: 페이지네이션 동적 쿼리 변경
    - 기존에는 동적 쿼리에서는 오직 <if> 엘리먼트만 이용하여 조건 및 필터링을 WHERE절에 추가하였지만, <trim> 엘리먼트를 이용하여 간편하게 WHERE절을 구성할 수 있게 함

--- src/main/java/koreatech/in/dto/shop/request/ShopsConditionDTO.java
+++ src/main/java/koreatech/in/dto/shop/request/ShopsConditionDTO.java
@@ -1,14 +1,12 @@
 package koreatech.in.dto.shop.request;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Getter @Setter
 public class ShopsConditionDTO {
@@ -20,19 +18,13 @@ public class ShopsConditionDTO {
     @NotNull(message = "limit는 필수입니다.")
     private Integer limit;
 
-    private Boolean is_deleted;
-    private Boolean delivery;
-    private Boolean pay_card;
-    private Boolean pay_bank;
+    private List<Filter> filter = new ArrayList<>();
 
     private Integer category_id;
 
     private Sort sort;
 
     private String search_name;
-
-    @ApiModelProperty(hidden = true)
-    private List<Filter> filter = new LinkedList<>();
 
     @Getter
     private enum Filter {
@@ -68,25 +60,11 @@ public class ShopsConditionDTO {
         return this.limit * this.page - this.limit;
     }
 
+    // 검색을 위해서 공백은 전부 제거해놓는 메소드
     public ShopsConditionDTO removeBlankOfSearchName() {
         if (this.search_name != null) {
             this.search_name = this.search_name.replace(" ", "").toLowerCase();
         }
         return this;
-    }
-
-    public void setFilter() {
-        if (Objects.equals(this.is_deleted, true)) {
-            filter.add(Filter.IS_DELETED);
-        }
-        if (Objects.equals(this.delivery, true)) {
-            filter.add(Filter.DELIVERY);
-        }
-        if (Objects.equals(this.pay_card, true)) {
-            filter.add(Filter.PAY_CARD);
-        }
-        if (Objects.equals(this.pay_bank, true)) {
-            filter.add(Filter.PAY_BANK);
-        }
     }
 }
