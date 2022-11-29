9b23518667c579bfef959ae151b50c0504a56a6f
Kim-Juwon <damiano102777@naver.com>
2022-11-29 21:14:05 +0900

    refactor: member 단건 조회 쿼리를 LEFT JOIN으로 변경
    - 기존 MemberMapper의 getMemberForAdmin() 메소드의 쿼리 방식은 INNER JOIN이기 때문에, 트랙 정보가 없으면 멤버 정보를 불러오지 못하는 현상 발생
    - 따라서 LEFT JOIN으로 변경하여 트랙이 없더라도 멤버를 불러올 수 있게 함

--- src/main/java/koreatech/in/repository/MemberMapper.java
+++ src/main/java/koreatech/in/repository/MemberMapper.java
@@ -30,8 +30,21 @@ public interface MemberMapper {
             " FROM koin.members AS t1, koin.tracks AS t2 WHERE t1.track_id = #{id} AND t2.id = #{id}")
     List<Member> getTrackMembersForAdmin(@Param("id") int id);
 
-    @Select("SELECT t1.id, t1.name, t1.student_number, t2.name AS track, t1.position, t1.email, t1.image_url, t1.is_deleted, t1.created_at, t1.updated_at" +
-            " FROM koin.members AS t1, koin.tracks AS t2 WHERE t1.id = #{id} AND t2.id = t1.track_id")
+    @Select("SELECT " +
+                "m.id AS id, " +
+                "m.`name` AS `name`, " +
+                "m.student_number AS student_number, " +
+                "t.`name` AS track, " +
+                "m.`position` AS `position`, " +
+                "m.`email` AS `email`, " +
+                "m.image_url AS image_url, " +
+                "m.is_deleted AS is_deleted, " +
+                "m.created_at AS created_at, " +
+                "m.updated_at AS updated_at " +
+            "FROM koin.members m " +
+                "LEFT JOIN koin.tracks t " +
+                "ON m.track_id = t.id " +
+            "WHERE m.id = #{id}")
     Member getMemberForAdmin(@Param(value = "id") int id);
 
     @Insert("INSERT INTO koin.members (name, student_number, track_id, position, email, image_url)" +
