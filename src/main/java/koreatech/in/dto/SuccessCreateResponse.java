724e4da91aa7f8e7ea4c7f6e29700513f24c5eb7
Kim-Juwon <damiano102777@naver.com>
2022-12-08 22:31:24 +0900

    docs: SuccessResponse, SuccessCreateResponse 설명 추가

--- src/main/java/koreatech/in/dto/SuccessCreateResponse.java
+++ src/main/java/koreatech/in/dto/SuccessCreateResponse.java
@@ -1,9 +1,11 @@
 package koreatech.in.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @Getter @SuperBuilder
 public class SuccessCreateResponse extends SuccessResponse {
+    @ApiModelProperty(notes = "생성된 레코드의 고유 id", example = "1", required = true)
     private Integer id; // 생성된 레코드 id
 }
