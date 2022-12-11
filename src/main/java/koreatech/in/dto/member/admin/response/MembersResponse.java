13b8805f0ec2cb8d9fdfcdc9d273e0f43d88b8f4
Kim-Juwon <damiano102777@naver.com>
2022-12-11 21:49:56 +0900

    docs: not null 파라미터에 required=true 표시

--- src/main/java/koreatech/in/dto/member/admin/response/MembersResponse.java
+++ src/main/java/koreatech/in/dto/member/admin/response/MembersResponse.java
@@ -8,36 +8,36 @@ import java.util.List;
 
 @Getter @Builder
 public class MembersResponse {
-    @ApiModelProperty(notes = "조건에 해당하는 총 회원의 수", example = "57")
+    @ApiModelProperty(notes = "조건에 해당하는 총 회원의 수", example = "57", required = true)
     private Integer total_count;
 
-    @ApiModelProperty(notes = "조건에 해당하는 회원 중에 현재 페이지에서 조회된 수", example = "10")
+    @ApiModelProperty(notes = "조건에 해당하는 회원 중에 현재 페이지에서 조회된 수", example = "10", required = true)
     private Integer current_count;
 
-    @ApiModelProperty(notes = "조건에 해당하는 회원들을 조회할 수 있는 최대 페이지", example = "6")
+    @ApiModelProperty(notes = "조건에 해당하는 회원들을 조회할 수 있는 최대 페이지", example = "6", required = true)
     private Integer total_page;
 
-    @ApiModelProperty(notes = "현재 페이지", example = "2")
+    @ApiModelProperty(notes = "현재 페이지", example = "2", required = true)
     private Integer current_page;
 
-    @ApiModelProperty(notes = "회원 정보 리스트")
+    @ApiModelProperty(notes = "회원 정보 리스트", required = true)
     private List<Member> members;
 
     @Getter
     public static final class Member {
-        @ApiModelProperty(notes = "고유 id", example = "1")
+        @ApiModelProperty(notes = "고유 id", example = "1", required = true)
         private Integer id;
 
-        @ApiModelProperty(notes = "이름", example = "김주원")
+        @ApiModelProperty(notes = "이름", example = "김주원", required = true)
         private String name;
 
         @ApiModelProperty(notes = "학번", example = "2019136037")
         private String student_number;
 
-        @ApiModelProperty(notes = "트랙 이름", example = "BackEnd")
+        @ApiModelProperty(notes = "트랙 이름", example = "BackEnd", required = true)
         private String track;
 
-        @ApiModelProperty(notes = "직책", example = "Regular")
+        @ApiModelProperty(notes = "직책", example = "Regular", required = true)
         private String position;
 
         @ApiModelProperty(notes = "이메일", example = "damiano102777@naver.com")
