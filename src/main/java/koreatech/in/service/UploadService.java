a26412f4ef3de5859ae613fc87b65edb5f160edb
Invidam <hansupark0208@gmail.com>
2023-01-13 22:38:12 +0900

    refactor: UploadService 매개변수 변경

--- src/main/java/koreatech/in/service/UploadService.java
+++ src/main/java/koreatech/in/service/UploadService.java
@@ -1,10 +1,11 @@
 package koreatech.in.service;
 
+import koreatech.in.domain.Upload.UploadFile;
 import koreatech.in.domain.Upload.UploadFileUrl;
 
 public interface UploadService {
-    void uploadFile(String uploadPath, String originalFileName, byte[] byteData);
+    void uploadFile(UploadFile uploadFile);
 
-    UploadFileUrl getUploadFileUrl(String uploadPath, String originalFileName, byte[] byteData);
+    UploadFileUrl getUploadFileUrl(UploadFile uploadFile);
 
 }
