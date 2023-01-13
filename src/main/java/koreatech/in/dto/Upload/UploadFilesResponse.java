4a70f4ffe79c3b865de3491555f05d376a805f53
Invidam <hansupark0208@gmail.com>
2023-01-13 22:42:13 +0900

    refactor: UploadImagesResponse 구조 변경
    - 이름 변경 (UploadImagesResponse -> UploadFilesResponse)
    - 패키지 변경 (~/dto -> ~/dto/Upload)
    - 생성자 롬복 변경 (Builder -> RequiredArgsConstructor)

---
+++ src/main/java/koreatech/in/dto/Upload/UploadFilesResponse.java
@@ -0,0 +1,12 @@
+package koreatech.in.dto.Upload;
+
+import io.swagger.annotations.ApiModelProperty;
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter @RequiredArgsConstructor(staticName = "from")
+public class UploadFilesResponse {
+    @ApiModelProperty(notes = "업로드된 파일 url 리스트", example = "[\"https://static.koreatech.in/example1.png\", \"https://static.koreatech.in/example2.png\"]", required = true)
+    private final List<String> file_urls;
+}
