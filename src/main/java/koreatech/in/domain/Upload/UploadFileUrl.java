04f9d93b5f949fbbb78a4d7b648e30c314d83d6a
Invidam <hansupark0208@gmail.com>
2023-01-13 14:37:42 +0900

    feat: UploadFileUrl 도메인 클래스 정의

---
+++ src/main/java/koreatech/in/domain/Upload/UploadFileUrl.java
@@ -0,0 +1,16 @@
+package koreatech.in.domain.Upload;
+
+import koreatech.in.dto.UploadFileResponse;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(staticName = "from")
+public class UploadFileUrl {
+
+    private final String fileUrl;
+
+    public UploadFileResponse toDTO() {
+        return UploadFileResponse.from(fileUrl);
+    }
+}
