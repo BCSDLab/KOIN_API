047ffa90b0cce378c6bfb0aa18949b7e138aca55
Invidam <hansupark0208@gmail.com>
2023-01-16 17:46:54 +0900

    feat: 유효성 검증 로직 추가

--- src/main/java/koreatech/in/dto/upload/request/UploadFilesRequest.java
+++ src/main/java/koreatech/in/dto/upload/request/UploadFilesRequest.java
@@ -3,20 +3,23 @@ package koreatech.in.dto.upload.request;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import koreatech.in.exception.BaseException;
+import koreatech.in.exception.ExceptionInformation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 public class UploadFilesRequest {
+    private final static int FILE_MAX_SIZE = 10;
     private final List<UploadFileRequest> uploadFilesRequest;
 
     public List<UploadFileRequest> getUploadFilesRequest() {
         return Collections.unmodifiableList(uploadFilesRequest);
     }
 
-
     public static UploadFilesRequest of(List<MultipartFile> files,
                                                              String domain) {
+        validation(files);
 
         List<UploadFileRequest> uploadFileRequests = new ArrayList<>();
 
@@ -25,5 +28,22 @@ public class UploadFilesRequest {
         }
 
         return new UploadFilesRequest(uploadFileRequests);
+    }
+
+    private static void validation(List<MultipartFile> files) {
+        validateEmpty(files);
+        validateLengthOver(files);
+    }
+
+    private static void validateEmpty(List<MultipartFile> files) {
+        if(files == null || files.isEmpty()) {
+            throw new BaseException(ExceptionInformation.FILES_EMPTY);
+        }
+    }
+
+    private static void validateLengthOver(List<MultipartFile> files) {
+        if(files.size() > FILE_MAX_SIZE) {
+            throw new BaseException(ExceptionInformation.FILES_LENGTH_OVER);
+        }
     }
 }
