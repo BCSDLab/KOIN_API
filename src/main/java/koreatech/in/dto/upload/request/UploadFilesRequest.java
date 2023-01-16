e26f3acc9e80b8d414a941daa20d04b3bd1160b8
Invidam <hansupark0208@gmail.com>
2023-01-16 17:33:58 +0900

    refactor: 팩터리 메서드 로직 변경

--- src/main/java/koreatech/in/dto/upload/request/UploadFilesRequest.java
+++ src/main/java/koreatech/in/dto/upload/request/UploadFilesRequest.java
@@ -1,14 +1,29 @@
 package koreatech.in.dto.upload.request;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
-@RequiredArgsConstructor(staticName = "from")
+@RequiredArgsConstructor
 public class UploadFilesRequest {
     private final List<UploadFileRequest> uploadFilesRequest;
 
     public List<UploadFileRequest> getUploadFilesRequest() {
         return Collections.unmodifiableList(uploadFilesRequest);
+    }
+
+
+    public static UploadFilesRequest from(List<MultipartFile> files,
+                                                             String domain) {
+
+        List<UploadFileRequest> uploadFileRequests = new ArrayList<>();
+
+        for (MultipartFile multipartFile : files) {
+            uploadFileRequests.add(UploadFileRequest.of(domain, multipartFile));
+        }
+
+        return new UploadFilesRequest(uploadFileRequests);
     }
 }
