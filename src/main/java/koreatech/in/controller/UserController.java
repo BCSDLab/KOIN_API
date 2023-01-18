165f1ea1b7cbf3fce29404f5cd324d121471d955
김주원 <damiano102777@naver.com>
2023-01-18 20:15:32 +0900

    docs: Swagger error code 설명을 마크다운 리스트로 표현

--- src/main/java/koreatech/in/controller/UserController.java
+++ src/main/java/koreatech/in/controller/UserController.java
@@ -116,9 +116,9 @@ public class UserController {
 
     @ApiOperation(value = "탈퇴", authorizations = {@Authorization("Authorization")})
     @ApiResponses({
-            @ApiResponse(code = 401, message = "잘못된 접근일 때 (code: 100001) \n\n" +
-                                               "토큰의 유효시간이 만료되었을 때 (code: 100004) \n\n" +
-                                               "토큰이 변경되었을 때 (code: 100005)", response = ExceptionResponse.class)
+            @ApiResponse(code = 401, message = "- 잘못된 접근일 때 (code: 100001) \n\n" +
+                                               "- 토큰의 유효시간이 만료되었을 때 (code: 100004) \n\n" +
+                                               "- 토큰이 변경되었을 때 (code: 100005)", response = ExceptionResponse.class)
     })
     @RequestMapping(value = "/user", method = RequestMethod.DELETE)
     public @ResponseBody
