20d2b850eda1021e942f6be7d21533582bb97d02
Invidam <hansupark0208@gmail.com>
2023-02-03 20:21:44 +0900

    feat: OwnerService, OwnerServiceImpl 서비스 객체 정의

---
+++ src/main/java/koreatech/in/service/OwnerServiceImpl.java
@@ -0,0 +1,12 @@
+package koreatech.in.service;
+
+import koreatech.in.dto.normal.owner.request.VerifyEmailRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OwnerServiceImpl implements OwnerService{
+    @Override
+    public void requestVerification(VerifyEmailRequest verifyEmailRequest) {
+
+    }
+}
