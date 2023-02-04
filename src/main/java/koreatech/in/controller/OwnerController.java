5c624f13e6b817aad65fab70a25c1cbcb8192a2a
Invidam <hansupark0208@gmail.com>
2023-02-04 18:23:54 +0900

    refactor: 이메일 인증 예외 Swagger 응답 어노테이션에 반영

--- src/main/java/koreatech/in/controller/OwnerController.java
+++ src/main/java/koreatech/in/controller/OwnerController.java
@@ -3,13 +3,17 @@ package koreatech.in.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.Authorization;
 import javax.validation.Valid;
 import koreatech.in.annotation.Auth;
 import koreatech.in.annotation.Auth.Authority;
 import koreatech.in.annotation.Auth.Role;
 import koreatech.in.annotation.AuthExcept;
+import koreatech.in.annotation.ParamValid;
 import koreatech.in.dto.EmptyResponse;
+import koreatech.in.dto.ExceptionResponse;
 import koreatech.in.dto.normal.owner.request.VerifyEmailRequest;
 import koreatech.in.service.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,8 +35,15 @@ public class OwnerController {
     private OwnerService ownerService;
 
     @AuthExcept
+    @ApiResponses({
+            @ApiResponse(code = 422, message = "- 이메일 주소가 올바르지 않을 경우 (code: 122000) \n\n" +
+                    "- 이메일 도메인이 사용할 수 없는 경우 (code: 122001) \n\n" +
+                    "- 탈퇴하지 않은 회원 중, 같은 이메일을 가지고 있는 회원이 있어서 탈퇴를 해제할 수 없는 경우 (code: 101007)\n\n",
+                    response = ExceptionResponse.class),
+    })
     @ApiOperation(value = "인증번호 전송 요청", authorizations = {@Authorization("Authorization")})
     @RequestMapping(value = "/owners/verficitation/email", method = RequestMethod.POST)
+    @ParamValid
     public @ResponseBody
     ResponseEntity<EmptyResponse> verifyEmail(@RequestBody @Valid VerifyEmailRequest request, BindingResult bindingResult) {
         //TODO 23.02.04. request body안에 있는 이메일, XSS 검증 필요한지
