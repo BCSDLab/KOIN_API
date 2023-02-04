ac6e77415aa9329ec3a3290af8086395120f0790
Invidam <hansupark0208@gmail.com>
2023-02-04 21:50:43 +0900

    refactor: swagger에 제약조건까지 명시

--- src/main/java/koreatech/in/dto/normal/owner/request/VerifyEmailRequest.java
+++ src/main/java/koreatech/in/dto/normal/owner/request/VerifyEmailRequest.java
@@ -10,6 +10,8 @@ public class VerifyEmailRequest {
 
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     @NotNull(message = "이메일은 필수입니다.")
-    @ApiModelProperty(notes = "이메일 주소", required = true)
+    @ApiModelProperty(notes = "이메일 주소 \n" +
+            "- not null \n" +
+            "- 이메일 형식이어야 함", required = true)
     private String address;
 }
