b498521a50f4e5c9065c92b4068952087f730ccd
Invidam <hansupark0208@gmail.com>
2023-02-05 00:41:46 +0900

    refactor: Redis 내에 있던 객체 검증 예외 swagger에 추가

--- src/main/java/koreatech/in/controller/OwnerController.java
+++ src/main/java/koreatech/in/controller/OwnerController.java
@@ -5,7 +5,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.Authorization;
 import javax.validation.Valid;
 import koreatech.in.annotation.Auth;
 import koreatech.in.annotation.Auth.Authority;
@@ -38,13 +37,13 @@ public class OwnerController {
     @Autowired
     private OwnerService ownerService;
 
-    @AuthExcept
     @ApiResponses({
             @ApiResponse(code = 422, message = "- 이메일 주소가 올바르지 않을 경우 (code: 101008) \n\n" +
                     "- 이메일 도메인이 사용할 수 없는 경우 (code: 101009) \n\n",
                     response = ExceptionResponse.class),
     })
-    @ApiOperation(value = "인증번호 전송 요청", authorizations = {@Authorization("Authorization")})
+    @ApiOperation(value = "인증번호 전송 요청")
+    @AuthExcept
     @RequestMapping(value = "/owners/verification/email", method = RequestMethod.POST)
     @ParamValid
     public @ResponseBody
@@ -60,12 +59,24 @@ public class OwnerController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 409, message = "- 요청받지 않은 이메일일 경우 (code: 101010) \n\n",
+                    response = ExceptionResponse.class),
+            @ApiResponse(code = 409, message = "- 이미 인증이 완료된 이메일일 경우 (code: 121000) \n\n",
+                    response = ExceptionResponse.class),
+            @ApiResponse(code = 410, message = "- 코드 인증 기한(`5분`)이 경과된 경우 (code: 121001) \n\n",
+                    response = ExceptionResponse.class),
+            @ApiResponse(code = 422, message = "- 인증 코드가 일치하지 않을 경우 (code: 121002) \n\n" +
+                    "- 이메일 도메인이 사용할 수 없는 경우 (code: 101009) \n\n",
+                    response = ExceptionResponse.class),
+    })
+    @ApiOperation(value = "인증번호 입력")
     @AuthExcept
     @RequestMapping(value = "/owners/verification/code", method = RequestMethod.POST)
     @ParamValid
     public @ResponseBody
     ResponseEntity<EmptyResponse> verifyCode(@RequestBody @Valid VerifyCodeRequest request,
-                                              BindingResult bindingResult) {
+                                             BindingResult bindingResult) {
         try {
             request = StringXssChecker.xssCheck(request, new VerifyCodeRequest());
         } catch (Exception exception) {
