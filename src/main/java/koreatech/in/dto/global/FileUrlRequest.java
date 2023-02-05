99d963b6635c72579611ea0d7e7314d8b0becf38
Invidam <hansupark0208@gmail.com>
2023-02-05 20:29:04 +0900

    feat: 파일 업로드에서 Owner Domain 추가 swagger에 반영

---
+++ src/main/java/koreatech/in/dto/global/FileUrlRequest.java
@@ -0,0 +1,19 @@
+package koreatech.in.dto.global;
+
+import io.swagger.annotations.ApiModelProperty;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
+
+@Getter
+@Setter
+public class FileUrlRequest {
+    @NotBlank
+    @URL(protocol = "https", host = "static.koreatech.in"
+            , message = "코인 파일 저장 형식이 아닙니다.")
+    @ApiModelProperty(notes = " \n" +
+            "- not blank \n" +
+            "- 코인 파일 저장 형식이어야 함", required = true)
+    private String fileUrl;
+}
