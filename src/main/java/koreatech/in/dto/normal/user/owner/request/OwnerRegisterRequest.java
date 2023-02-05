9bb8b63d106cd74919db8928ba50c7c8240372fd
Invidam <hansupark0208@gmail.com>
2023-02-05 20:26:40 +0900

    feat: 회원가입 위한 DTO 정의
    - Student, Owner의 공통부분을 맡는 UserRegisterRequest
    - Owner 회원가입에 해당하는 OwnerRegisterRequest

---
+++ src/main/java/koreatech/in/dto/normal/user/owner/request/OwnerRegisterRequest.java
@@ -0,0 +1,38 @@
+package koreatech.in.dto.normal.user.owner.request;
+
+import io.swagger.annotations.ApiModelProperty;
+import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import koreatech.in.dto.global.FileUrlRequest;
+import koreatech.in.dto.normal.user.request.UserRegisterRequest;
+
+
+public class OwnerRegisterRequest extends UserRegisterRequest {
+
+    @NotBlank(message = "사업자 등록 번호는 필수입니다.")
+    @Pattern(regexp = "^[0-9]{3}-[0-9]{2}-[0-9]{5}", message = "사업자 등록 번호 형식이 올바르지 않습니다.")
+    @ApiModelProperty(notes = "사업자 등록 번호 \n"
+            + "- not blank \n"
+            + "- 사업자 등록 번호 형식이어야 함"
+            , example = "012-34-56789"
+            , required = true
+    )
+    private String companyNumber;
+
+    //TODO 23.02.05 박한수 10mb 이하 이미지 파일만 허용 어떻게 처리할 것인지.
+    @NotNull(message = "이미지 첨부는 필수입니다.")
+    @Size(min = 3, max = 5, message = "이미지는 사업자등록증, 영업신고증, 통장사본을 포함하여 최대 5개까지 가능합니다.")
+    @ApiModelProperty(notes = "첨부 이미지들 \n"
+            + "- not null \n"
+            + "- 이미지는 최소 3개 최대 5개까지 허용됨 \n"
+            + "- 모든 이미지들이 코인 이미지 형식이어야 함 \n"
+            , example = "012-34-56789"
+            , required = true
+    )
+    @Valid
+    private List<FileUrlRequest> companyCertificateFileUrls;
+}
