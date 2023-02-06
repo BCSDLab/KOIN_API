02820dcbd34b45a79f3d1822aa7ba33b7b3a4ceb
김주원 <damiano102777@naver.com>
2023-02-06 18:18:18 +0900

    feat: 메뉴 카테고리 생성 dto 정의

---
+++ src/main/java/koreatech/in/dto/normal/shop/request/CreateMenuCategoryRequest.java
@@ -0,0 +1,18 @@
+package koreatech.in.dto.normal.shop.request;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter @Setter
+public class CreateMenuCategoryRequest {
+    @Size(min = 1, max = 15, message = "name은 1자 이상 15자 이하입니다.")
+    @NotNull(message = "name은 필수입니다.")
+    @ApiModelProperty(notes = "카테고리명 \n" +
+                              "- not null \n" +
+                              "- 1자 이상 15자 이하", example = "사이드 메뉴", required = true)
+    private String name;
+}
