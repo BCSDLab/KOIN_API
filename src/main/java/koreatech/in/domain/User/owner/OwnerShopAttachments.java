d9d08135693d34229297f019e4bba81f868a1647
Invidam <hansupark0208@gmail.com>
2023-02-07 01:06:24 +0900

    feat: 첨부파일 저장위한 entity 객체 정의

---
+++ src/main/java/koreatech/in/domain/User/owner/OwnerShopAttachments.java
@@ -0,0 +1,11 @@
+package koreatech.in.domain.User.owner;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OwnerShopAttachments {
+    private List<OwnerShopAttachment> attachments;
+}
