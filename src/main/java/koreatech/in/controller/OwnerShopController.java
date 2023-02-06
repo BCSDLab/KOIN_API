2659de6c265af079a44caf0511cc67c637c1038a
김주원 <damiano102777@naver.com>
2023-02-07 00:28:31 +0900

    docs: Swagger 설명 추가

--- src/main/java/koreatech/in/controller/OwnerShopController.java
+++ src/main/java/koreatech/in/controller/OwnerShopController.java
@@ -18,14 +18,14 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @Auth(role = Auth.Role.OWNER, authority = Auth.Authority.SHOP)
-@Api(tags = "(Normal) Owner Shop", description = "상점 (사장님 전용)")
+@Api(tags = "(Normal) Owner Shop", description = "상점 (점주 전용)")
 @Controller
 @RequestMapping("/owner/shops")
 public class OwnerShopController {
     @Autowired
     private OwnerShopService ownerShopService;
 
-    @ApiOperation(value = "메뉴 카테고리 생성", notes = "한 상점당 최대 20개까지 등록 가능", authorizations = {@Authorization("Authorization")})
+    @ApiOperation(value = "상점에 메뉴 카테고리 생성", notes = "인증 정보에 대한 신원이 상점의 점주가 아니라면 403(Forbidden) 응답", authorizations = {@Authorization("Authorization")})
     @ApiResponses({
             @ApiResponse(code = 401, message = "- 잘못된 접근일 때 (code: 100001) \n" +
                                                "- 액세스 토큰이 만료되었을 때 (code: 100004) \n" +
@@ -41,13 +41,13 @@ public class OwnerShopController {
     @RequestMapping(value = "/{id}/menus/categories", method = RequestMethod.POST)
     public @ResponseBody
     ResponseEntity<EmptyResponse> createMenuCategory(
-            @ApiParam(name = "상점 고유 id", required = true) @PathVariable("id") Integer shopId,
+            @ApiParam(required = true) @PathVariable("id") Integer shopId,
             @ApiParam(name = "메뉴 카테고리 정보 JSON", required = true) @RequestBody @Valid CreateMenuCategoryRequest request, BindingResult bindingResult) {
         ownerShopService.createMenuCategory(shopId, request);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @ApiOperation(value = "상점의 모든 메뉴 카테고리 조회", authorizations = {@Authorization("Authorization")})
+    @ApiOperation(value = "상점의 모든 메뉴 카테고리 조회", notes = "인증 정보에 대한 신원이 상점의 점주가 아니라면 403(Forbidden) 응답", authorizations = {@Authorization("Authorization")})
     @ApiResponses({
             @ApiResponse(code = 401, message = "- 잘못된 접근일 때 (code: 100001) \n" +
                                                "- 액세스 토큰이 만료되었을 때 (code: 100004) \n" +
@@ -55,14 +55,14 @@ public class OwnerShopController {
             @ApiResponse(code = 403, message = "- 권한이 없을 때 (code: 100003)", response = ExceptionResponse.class),
             @ApiResponse(code = 404, message = "- 상점이 존재하지 않을 때 (code: 104000)", response = ExceptionResponse.class)
     })
-    @RequestMapping(value = "{id}/menus/categories", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}/menus/categories", method = RequestMethod.GET)
     public @ResponseBody
-    ResponseEntity<AllMenuCategoriesOfShopResponse> getAllMenuCategoriesOfShop(@ApiParam(name = "상점 고유 id", required = true) @PathVariable("id") Integer shopId) {
+    ResponseEntity<AllMenuCategoriesOfShopResponse> getAllMenuCategoriesOfShop(@ApiParam(required = true) @PathVariable("id") Integer shopId) {
         AllMenuCategoriesOfShopResponse response = ownerShopService.getAllMenuCategoriesOfShop(shopId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @ApiOperation(value = "상점의 메뉴 카테고리 삭제", authorizations = {@Authorization("Authorization")})
+    @ApiOperation(value = "상점의 메뉴 카테고리 삭제", notes = "인증 정보에 대한 신원이 상점의 점주가 아니라면 403(Forbidden) 응답", authorizations = {@Authorization("Authorization")})
     @ApiResponses({
             @ApiResponse(code = 401, message = "- 잘못된 접근일 때 (code: 100001) \n" +
                                                "- 액세스 토큰이 만료되었을 때 (code: 100004) \n" +
@@ -73,11 +73,11 @@ public class OwnerShopController {
                                                "  - 만약 categoryId에 대한 카테고리가, shopId에 대한 상점에 속해있는 카테고리가 아닌 경우도 포함", response = ExceptionResponse.class),
             @ApiResponse(code = 409, message = "- 해당 카테고리를 사용하고 있는 메뉴가 존재하여 삭제할 수 없는 경우 (code: 104012)", response = ExceptionResponse.class)
     })
-    @RequestMapping(value = "{shopId}/menus/categories/{categoryId}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/{shopId}/menus/categories/{categoryId}", method = RequestMethod.DELETE)
     public @ResponseBody
     ResponseEntity<EmptyResponse> deleteMenuCategory(
-            @ApiParam(name = "상점 고유 id", required = true) @PathVariable("shopId") Integer shopId,
-            @ApiParam(name = "메뉴 카테고리 고유 id", required = true) @PathVariable("categoryId") Integer menuCategoryId) {
+            @ApiParam(required = true) @PathVariable("shopId") Integer shopId,
+            @ApiParam(required = true) @PathVariable("categoryId") Integer menuCategoryId) {
         ownerShopService.deleteMenuCategory(shopId, menuCategoryId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
