9f36e74344d524c8bc10be4d8e0c94d1bfb7530a
김주원 <damiano102777@naver.com>
2023-02-07 20:41:34 +0900

    feat: 상점 카테고리 이미지 url에 https:// 추가

--- src/main/resources/db/migration/V56__create_shop_categories_table.sql
+++ src/main/resources/db/migration/V56__create_shop_categories_table.sql
@@ -11,14 +11,14 @@ ENGINE = InnoDB
 DEFAULT CHARSET = utf8
 COLLATE = utf8_bin;
 
-INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('전체보기', 'static.koreatech.in/assets/img/img-rest-etc.png');
-INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('치킨', 'static.koreatech.in/assets/img/img-rest-chicken.png');
-INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('피자', 'static.koreatech.in/assets/img/img-rest-pizza.png');
-INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('도시락', 'static.koreatech.in/assets/img/img-rest-box.png');
-INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('족발', 'static.koreatech.in/assets/img/img-rest-porkfeet.png');
-INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('중국집', 'static.koreatech.in/assets/img/img-rest-blacknoodle.png');
-INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('일반음식점', 'static.koreatech.in/assets/img/img-rest-normal.png');
-INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('카페', 'static.koreatech.in/assets/img/img-rest-cafe.png');
-INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('미용실', 'static.koreatech.in/assets/img/img-rest-salon.png');
-INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('기타', 'static.koreatech.in/assets/img/img-rest-etc.png');
+INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('전체보기', 'https://static.koreatech.in/assets/img/img-rest-etc.png');
+INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('치킨', 'https://static.koreatech.in/assets/img/img-rest-chicken.png');
+INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('피자', 'https://static.koreatech.in/assets/img/img-rest-pizza.png');
+INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('도시락', 'https://static.koreatech.in/assets/img/img-rest-box.png');
+INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('족발', 'https://static.koreatech.in/assets/img/img-rest-porkfeet.png');
+INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('중국집', 'https://static.koreatech.in/assets/img/img-rest-blacknoodle.png');
+INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('일반음식점', 'https://static.koreatech.in/assets/img/img-rest-normal.png');
+INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('카페', 'https://static.koreatech.in/assets/img/img-rest-cafe.png');
+INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('미용실', 'https://static.koreatech.in/assets/img/img-rest-salon.png');
+INSERT INTO `koin`.`shop_categories` (`name`, `image_url`) VALUES ('기타', 'https://static.koreatech.in/assets/img/img-rest-etc.png');
 
