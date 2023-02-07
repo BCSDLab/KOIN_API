b8e53d0a04c27247ff77875fdadf71c7de7a3e52
Invidam <hansupark0208@gmail.com>
2023-02-07 15:18:23 +0900

    feat: Notification 관련 문자열 static 처리

--- src/main/java/koreatech/in/domain/NotiSlack.java
+++ src/main/java/koreatech/in/domain/NotiSlack.java
@@ -6,6 +6,12 @@ import lombok.Getter;
 @Getter
 @Builder
 public class NotiSlack {
+    public static final String EMAIL_VERIFICATION_REQUEST_SUFFIX = "님이 이메일 인증을 요청하였습니다.";
+    public static final String REGISTER_COMPLETE_SUFFIX = "님이 가입하셨습니다.";
+
+    public static final String COLOR_GOOD = "good";
+
+
     private String color;
 
     private String author_name;
