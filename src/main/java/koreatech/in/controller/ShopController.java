e3eda5e4908fe7f68b50eb88ebde263514c197e4
김주원 <damiano102777@naver.com>
2023-02-07 19:13:20 +0900

    refactor: root uri를 controller 클래스 상단에 선언

--- src/main/java/koreatech/in/controller/ShopController.java
+++ src/main/java/koreatech/in/controller/ShopController.java
@@ -15,36 +15,33 @@ import javax.inject.Inject;
 
 @Api(tags = "(Normal) Shop", description = "상점")
 @Controller
+@RequestMapping("/shops")
 public class ShopController {
     @Inject
     private ShopService shopService;
 
-    /*
-         TODO: 카테고리 순서 확정 후 마이그레이션 SQL 변경,
-               페이지 방식으로 결정된다면 리팩토링
-     */
-    @RequestMapping(value = "/shops/categories", method = RequestMethod.GET)
+    @RequestMapping(value = "/categories", method = RequestMethod.GET)
     public @ResponseBody
     ResponseEntity<AllShopCategoriesResponse> getAllShopCategories() {
         AllShopCategoriesResponse response = shopService.getAllShopCategories();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/shops/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public @ResponseBody
     ResponseEntity<ShopResponse> getShop(@PathVariable("id") Integer shopId) {
         ShopResponse response = shopService.getShop(shopId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/shops", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public @ResponseBody
     ResponseEntity<AllShopsResponse> getAllShops() {
         AllShopsResponse response = shopService.getAllShops();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/shops/{id}/menus", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}/menus", method = RequestMethod.GET)
     public @ResponseBody
     ResponseEntity<AllMenusOfShopResponse> getAllMenusOfShop(@PathVariable("id") Integer shopId) {
         AllMenusOfShopResponse response = shopService.getAllMenusOfShop(shopId);
