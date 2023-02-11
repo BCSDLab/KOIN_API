ab6aef77b120cd2aaddcb6ac27488845ea2306a8
Invidam <hansupark0208@gmail.com>
2023-02-11 20:16:45 +0900

    feat: Controller 의 이메일 중복 체크 메서드 유효성 검증 위한 어노테이션 추가

--- src/main/java/koreatech/in/controller/UserController.java
+++ src/main/java/koreatech/in/controller/UserController.java
@@ -210,13 +210,14 @@ public class UserController {
     })
     @ApiOperation(value = "이메일 중복 체크")
     @AuthExcept
+    @ParamValid
     @RequestMapping(value = "/user/check/email/{address:.+}", method = RequestMethod.GET)
     public @ResponseBody
     ResponseEntity<EmptyResponse> checkUserEmailExist(
             @ApiParam(value = "이메일 \n " +
                     "- not null \n " +
                     "- 1자 이상 10자 이하 \n " +
-                    "- 공백 문자로만 이루어져있으면 안됨", required = true) @PathVariable("address") CheckExistsEmailRequest request) {
+                    "- 공백 문자로만 이루어져있으면 안됨", required = true) @PathVariable("address") @Valid CheckExistsEmailRequest request) {
         try {
             request = StringXssChecker.xssCheck(request, request.getClass().newInstance());
         } catch (Exception exception) {
