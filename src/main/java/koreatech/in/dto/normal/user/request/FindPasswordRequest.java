ca1915fe73a4b708f4806a5b9abf956f1ac7a9de
Invidam <hansupark0208@gmail.com>
2023-02-13 00:03:58 +0900

    feat: account -> email 대체 작업 DTO, Domain 객체에 적용

--- src/main/java/koreatech/in/dto/normal/user/request/FindPasswordRequest.java
+++ src/main/java/koreatech/in/dto/normal/user/request/FindPasswordRequest.java
@@ -1,6 +1,7 @@
 package koreatech.in.dto.normal.user.request;
 
 import io.swagger.annotations.ApiModelProperty;
+import javax.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +9,14 @@ import javax.validation.constraints.NotNull;
 
 @Getter @Setter
 public class FindPasswordRequest {
-    @NotNull(message = "account는 필수입니다.")
-    @ApiModelProperty(notes = "아이디 \n" +
-                              "- not null", example = "acsdefg1234", required = true)
-    private String account;
+
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @NotNull(message = "이메일은 필수입니다.")
+    @ApiModelProperty(notes = "이메일 주소 \n"
+            + "- not null \n"
+            + "- 이메일 형식이어야 함"
+            , required = true
+            , example = "abcd@gmail.com"
+    )
+    private String email;
 }
