b412455f271bba6266c65d6c2d12ec32ddad68af
Invidam <hansupark0208@gmail.com>
2023-02-12 12:22:28 +0900

    feat: 괄호를 통하여, 회원/사장님/학생 구분하도록 변경

--- src/main/java/koreatech/in/domain/NotiSlack.java
+++ src/main/java/koreatech/in/domain/NotiSlack.java
@@ -1,7 +1,8 @@
 package koreatech.in.domain;
 
-import koreatech.in.domain.User.EmailAddress;
+import koreatech.in.domain.User.User;
 import koreatech.in.domain.User.owner.Owner;
+import koreatech.in.domain.User.student.Student;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,6 +13,10 @@ public class NotiSlack {
     public static final String REGISTER_COMPLETE_SUFFIX = "님이 가입하셨습니다.";
 
     public static final String COLOR_GOOD = "good";
+
+    public static final String STUDENT_MESSAGE = "(학생)";
+    public static final String OWNER_MESSAGE = "(사장님)";
+    public static final String MEMBER_MESSAGE = "(회원)";
 
 
     private String color;
@@ -25,17 +30,33 @@ public class NotiSlack {
     private String text;
 
 
-    public static NotiSlack emailVerificationNotiSlack(EmailAddress emailAddress) {
+    public static NotiSlack emailVerificationNotiSlack(User user) {
         return NotiSlack.builder()
                 .color(NotiSlack.COLOR_GOOD)
-                .text(emailAddress.getEmailAddress() + NotiSlack.EMAIL_VERIFICATION_REQUEST_SUFFIX)
+                .text(makeText(user, NotiSlack.EMAIL_VERIFICATION_REQUEST_SUFFIX))
                 .build();
     }
 
-    public static NotiSlack registerCompleteNotiSlack(Owner owner) {
+    public static NotiSlack registerCompleteNotiSlack(User user) {
         return NotiSlack.builder()
                 .color(NotiSlack.COLOR_GOOD)
-                .text(owner.getEmail() + NotiSlack.REGISTER_COMPLETE_SUFFIX)
+                .text(makeText(user, NotiSlack.REGISTER_COMPLETE_SUFFIX))
                 .build();
     }
+
+    private static String makeText(User user, String messageSuffix) {
+        return user.getEmail() + DelimitedText(user) + messageSuffix;
+    }
+
+    private static String DelimitedText(User user) {
+        if(user instanceof Student) {
+            return STUDENT_MESSAGE;
+        }
+        if(user instanceof Owner) {
+            return OWNER_MESSAGE;
+        }
+
+        return MEMBER_MESSAGE;
+    }
+
 }
