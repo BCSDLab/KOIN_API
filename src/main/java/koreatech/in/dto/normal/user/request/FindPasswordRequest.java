ed4c96323d3272d51d58e2afce67ef14d95dd7ac
김주원 <damiano102777@naver.com>
2023-02-15 19:12:19 +0900

    fix: 요청 프로퍼티 변경
    - account에서 email로 변경

--- src/main/java/koreatech/in/dto/normal/user/request/FindPasswordRequest.java
+++ src/main/java/koreatech/in/dto/normal/user/request/FindPasswordRequest.java
@@ -4,12 +4,15 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 @Getter @Setter
 public class FindPasswordRequest {
-    @NotNull(message = "account는 필수입니다.")
-    @ApiModelProperty(notes = "아이디 \n" +
-                              "- not null", example = "acsdefg1234", required = true)
-    private String account;
+    @NotNull(message = "이메일은 필수입니다.")
+    @Email(message = "이메일 형식이어야 합니다.")
+    @ApiModelProperty(notes = "이메일 \n" +
+                              "- not null \n" +
+                              "- 이메일 형식이어야 함", example = "abc123@test.com", required = true)
+    private String email;
 }
