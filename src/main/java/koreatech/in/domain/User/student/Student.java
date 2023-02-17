9df2c24c9ea0b42675191cd4e832fc90de77e7c1
Invidam <hansupark0208@gmail.com>
2023-02-17 19:57:48 +0900

    feat: 익명 닉네임 메서드 객체 안으로 인라인

--- src/main/java/koreatech/in/domain/User/student/Student.java
+++ src/main/java/koreatech/in/domain/User/student/Student.java
@@ -10,6 +10,7 @@ import lombok.*;
 public class Student extends User {
     private String anonymous_nickname;
     private String student_number;
+    //TODO 23.02.17. 박한수 major -> department 변경이 필요할 듯. major 보단 department가 정확한 표현이라고 생각되기 때문. department: 학부 / major: 세부전공
     private String major;
 
     private Integer identity; // 신원(0: 학생, 1: 대학원생, 2: 교수, 3: 교직원, 4: 졸업생, 5: 점주)
@@ -54,6 +55,10 @@ public class Student extends User {
         if(student.is_graduated != null) {
             this.is_graduated = student.is_graduated;
         }
+    }
+
+    public void fillAnonymousNickname() {
+        this.setAnonymous_nickname("익명_" + (System.currentTimeMillis()));
     }
 
     @Builder
