b21294e8d3f7f86d2d7012e8c83b2e23554b3948
Invidam <hansupark0208@gmail.com>
2023-02-19 14:56:35 +0900

    feat: mapstruct 이용하여 student response 반환하도록 변경

--- src/main/java/koreatech/in/mapstruct/UserConverter.java
+++ src/main/java/koreatech/in/mapstruct/UserConverter.java
@@ -3,11 +3,16 @@ package koreatech.in.mapstruct;
 import koreatech.in.domain.User.Domain;
 import koreatech.in.domain.User.EmailAddress;
 import koreatech.in.domain.User.LocalParts;
+import koreatech.in.domain.User.User;
+import koreatech.in.domain.User.student.Student;
 import koreatech.in.dto.normal.user.request.CheckExistsEmailRequest;
+import koreatech.in.dto.normal.user.response.StudentResponse;
+import koreatech.in.dto.normal.user.response.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.Named;
+import org.mapstruct.SubclassMapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -31,5 +36,31 @@ public interface UserConverter {
     default Domain convertDomain(String address) {
         return Domain.from(address);
     }
+
+
+
+    @Mappings({
+            @Mapping(source = "id", target = "id"),
+            @Mapping(source = "email", target = "email"),
+            @Mapping(source = "nickname", target = "nickname"),
+            @Mapping(source = "name", target = "name"),
+            @Mapping(source = "phone_number", target = "phoneNumber"),
+            @Mapping(source = "gender", target = "gender"),
+            @Mapping(source = "is_authed", target = "isAuth"),
+            @Mapping(source = "user_type", target = "userType"),
+            @Mapping(source = "is_deleted", target = "isDeleted")
+    })
+    @SubclassMapping(source = Student.class, target = StudentResponse.class)
+    UserResponse toUserResponse(User user);
+
+    @Mappings({
+            @Mapping(source = "anonymous_nickname", target = "anonymousNickname"),
+            @Mapping(source = "student_number", target = "studentNumber"),
+            @Mapping(source = "major", target = "major"),
+            @Mapping(source = "identity", target = "identity"),
+            @Mapping(source = "is_graduated", target = "isGraduated"),
+            @Mapping(source = "phone_number", target = "phoneNumber")
+    })
+    StudentResponse toStudentResponse(Student student);
 
 }
