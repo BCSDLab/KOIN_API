5a100f19bb7cf113a3ac4c6d2ddd469fdf69e6d4
Invidam <hansupark0208@gmail.com>
2023-02-21 20:50:14 +0900

    refactor: 필드 명 변경

--- src/main/java/koreatech/in/dto/normal/upload/response/UploadFilesResponse.java
+++ src/main/java/koreatech/in/dto/normal/upload/response/UploadFilesResponse.java
@@ -7,10 +7,13 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class UploadFilesResponse {
-    @ApiModelProperty(notes = "업로드된 파일 url 리스트", example = "[\"https://static.koreatech.in/example1.png\", \"https://static.koreatech.in/example2.png\"]", required = true)
-    private final List<String> file_urls;
+    @ApiModelProperty(notes = "업로드된 파일 url 리스트",
+            dataType = "[Lkoreatech.in.dto.normal.upload.response.UploadFileResponse;"
+//            example = "[\"https://static.koreatech.in/example1.png\", \"https://static.koreatech.in/example2.png\"]", required = true
+            )
+    private final List<UploadFileResponse> filesResponse;
 
-    public List<String> getFile_urls() {
-        return Collections.unmodifiableList(file_urls);
+    public List<UploadFileResponse> getFilesResponse() {
+        return Collections.unmodifiableList(filesResponse);
     }
 }
