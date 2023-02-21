1aacaa935c746aa64e73cdb1745cc795f62474c7
Invidam <hansupark0208@gmail.com>
2023-02-21 20:49:56 +0900

    refactor: 필드 명 변경

--- src/main/java/koreatech/in/domain/Upload/UploadFilesResult.java
+++ src/main/java/koreatech/in/domain/Upload/UploadFilesResult.java
@@ -7,9 +7,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor(staticName = "from")
 public class UploadFilesResult {
-    private final List<UploadFileResult> uploadFileResults;
+    private final List<UploadFileResult> uploadFilesResult;
 
     public void append(UploadFileResult uploadFileResult) {
-        uploadFileResults.add(uploadFileResult);
+        uploadFilesResult.add(uploadFileResult);
     }
 }
