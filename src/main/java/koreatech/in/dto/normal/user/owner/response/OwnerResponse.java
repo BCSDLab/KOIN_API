9e662ea083fd613477da0b0629b500568beb3a22
Invidam <hansupark0208@gmail.com>
2023-02-24 23:45:33 +0900

    feat: Owner Response DTO 완성

--- src/main/java/koreatech/in/dto/normal/user/owner/response/OwnerResponse.java
+++ src/main/java/koreatech/in/dto/normal/user/owner/response/OwnerResponse.java
@@ -2,24 +2,19 @@ package koreatech.in.dto.normal.user.owner.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import javax.validation.Valid;
-import koreatech.in.dto.normal.shop.response.ShopResponse;
 import koreatech.in.dto.normal.user.response.UserResponse;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-@Getter
-@Setter
+@Getter @SuperBuilder
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@ApiModel("OwnerResponse")
 public class OwnerResponse extends UserResponse {
-
-    @ApiModelProperty(notes = "이름 "
-            , required = true
-            , example = "정보혁"
-    )
-    private String name;
 
     @ApiModelProperty(notes = "사업자 등록 번호 "
             , example = "012-34-56789"
@@ -27,15 +22,19 @@ public class OwnerResponse extends UserResponse {
     )
     private String companyNumber;
 
-//    @ApiModelProperty(notes = "첨부 이미지들 \n"
-//            , required = true
-//    )
-//    @Valid
-//    private List<AttachmentUrlRequest> attachmentUrls;
-
     @ApiModelProperty(notes = "가게 정보 \n"
             , required = true
     )
     @Valid
-    private List<ShopResponse> shops;
+    private List<Shop> shops;
+
+    @Getter @Builder
+    @ApiModel("Shop_in_OwnerResponse")
+    public static class Shop {
+        @ApiModelProperty(notes = "고유 id", example = "2", required = true)
+        private Integer id;
+
+        @ApiModelProperty(notes = "이름", example = "가장 맛있는 족발", required = true)
+        private String name;
+    }
 }
