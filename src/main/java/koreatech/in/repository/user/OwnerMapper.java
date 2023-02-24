382f04100d8c45e7fcc4d3b622f679cb9c0a434a
Invidam <hansupark0208@gmail.com>
2023-02-24 22:30:09 +0900

    feat: Repository 로직 작성

--- src/main/java/koreatech/in/repository/user/OwnerMapper.java
+++ src/main/java/koreatech/in/repository/user/OwnerMapper.java
@@ -2,14 +2,14 @@ package koreatech.in.repository.user;
 
 import koreatech.in.domain.User.owner.Owner;
 import koreatech.in.domain.User.owner.OwnerShopAttachments;
-import org.apache.ibatis.annotations.Select;
+import koreatech.in.domain.User.owner.OwnerWithShops;
 import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OwnerMapper {
-    @Select("SELECT * FROM koin.users AS user LEFT JOIN koin.users_owners AS owner ON owner.user_id = user.id WHERE user.id = #{id}")
-    Owner getOwnerById(Long id);
+//    @Select("SELECT * FROM koin.users AS user LEFT JOIN koin.users_owners AS owner ON owner.user_id = user.id WHERE user.id = #{id}")
+    OwnerWithShops getOwnerById(Long id);
     @Update("UPDATE koin.users_owners AS owner SET owner.email = #{email} WHERE owner.user_id = #{id}")
     void updateOwner(Owner owner);
     void insertOwner(Owner owner);
