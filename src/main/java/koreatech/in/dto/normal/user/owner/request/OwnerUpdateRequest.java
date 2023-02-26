7d659c283ca6862c1fd2f707b793cd16eff22e87
Invidam <hansupark0208@gmail.com>
2023-02-26 13:20:32 +0900

    feat: 빈 첨부파일도 가능하도록 수정

--- src/main/java/koreatech/in/dto/normal/user/owner/request/OwnerUpdateRequest.java
+++ src/main/java/koreatech/in/dto/normal/user/owner/request/OwnerUpdateRequest.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import koreatech.in.dto.global.AttachmentUrlRequest;
 import lombok.Getter;
@@ -18,14 +17,12 @@ import lombok.Setter;
 public class OwnerUpdateRequest // extends UserUpdateRequest // 사장님 Update에 대한 요청이 정확히 확인되지 않아, 필수적인 첨부파일만 넘기도록 함.
 {
 
-    @NotNull(message = "이미지 첨부는 필수입니다.")
     @Size(min = 3, max = 5, message = "이미지는 사업자등록증, 영업신고증, 통장사본을 포함하여 최소 3개 최대 5개까지 가능합니다.")
     @ApiModelProperty(notes = "첨부 이미지들 \n"
             + "- not null \n"
             + "- 이미지는 최소 3개 최대 5개까지 허용됨 \n"
             + "- 모든 이미지들이 코인 이미지 형식이어야 함 \n"
             , dataType = "[Lkoreatech.in.dto.global.AttachmentUrlRequest;"
-            , required = true
     )
     @Valid
     private List<AttachmentUrlRequest> attachmentUrls;
