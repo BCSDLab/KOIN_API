78cbc73898adbecf5cb028d7eb97256e6fba8947
김주원 <damiano102777@naver.com>
2023-02-28 16:03:57 +0900

    docs: Swagger 비밀번호 제약조건 설명 추가

--- src/main/java/koreatech/in/dto/normal/user/request/LoginRequest.java
+++ src/main/java/koreatech/in/dto/normal/user/request/LoginRequest.java
@@ -10,15 +10,13 @@ import lombok.Setter;
 public class LoginRequest {
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     @NotNull(message = "이메일은 필수입니다.")
-    @ApiModelProperty(notes = "이메일 주소 \n"
-            + "- not null \n"
-            + "- 이메일 형식이어야 함"
-            , required = true
-            , example = "abcd@gmail.com"
-    )
+    @ApiModelProperty(notes = "이메일 주소 \n" +
+                              "- not null \n" +
+                              "- 이메일 형식이어야 함 \n", required = true, example = "abcd@gmail.com")
     private String email;
 
     @NotNull(message = "비밀번호는 필수입니다.")
-    @ApiModelProperty(notes = "비밀번호", required = true)
+    @ApiModelProperty(notes = "비밀번호 \n" +
+                              "- not null", required = true)
     private String password;
 }
