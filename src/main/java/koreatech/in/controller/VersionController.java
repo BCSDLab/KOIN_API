593898322e5e1f3d9b3c206b703790776d4b7a94
Invidam <hansupark0208@gmail.com>
2023-04-08 17:15:23 +0900

    feat: VersionType 유효성 검증 로직 추가

--- src/main/java/koreatech/in/controller/VersionController.java
+++ src/main/java/koreatech/in/controller/VersionController.java
@@ -3,11 +3,11 @@ package koreatech.in.controller;
 import io.swagger.annotations.ApiParam;
 import javax.inject.Inject;
 import koreatech.in.domain.Version.Version;
+import koreatech.in.domain.Version.VersionTypeEnum;
 import koreatech.in.service.VersionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,12 +23,14 @@ public class VersionController {
     ResponseEntity<Version> getVersion(
             @PathVariable(value = "type")
             @ApiParam(value = "타입 이름 \n\n"
-                    + "- `android` (안드로이드)\n"
-                    + "- `shuttle_bus_timetable` (셔틀, 통학 버스)\n"
-                    + "- `express_bus_timetable` (대성 고속)\n"
-                    + "- `city_bus_timetable` (시내 버스)\n"
+                    + "- `android`(안드로이드)\n"
+                    + "- `shuttle_bus_timetable`(셔틀, 통학 버스)\n"
+                    + "- `express_bus_timetable`(대성 고속)\n"
+                    + "- `city_bus_timetable`(시내 버스)\n"
                     , example = "city_bus_timetable", required = true)
             String type) throws Exception {
+
+        VersionTypeEnum.validates(type);
 
         return new ResponseEntity<>(versionService.getVersion(type), HttpStatus.OK);
     }
