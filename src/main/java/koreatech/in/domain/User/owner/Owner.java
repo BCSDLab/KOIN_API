f0f4b98563c22f22d7ee856b192d60ed166ce3b9
JunYoung Jang <police0023@naver.com>
2023-04-13 10:19:00 +0900

    Owner

--- src/main/java/koreatech/in/domain/User/owner/Owner.java
+++ src/main/java/koreatech/in/domain/User/owner/Owner.java
@@ -1,9 +1,12 @@
 package koreatech.in.domain.User.owner;
 
 import java.util.List;
+import java.util.Objects;
+
 import koreatech.in.domain.Shop.Shop;
 import koreatech.in.domain.User.User;
 import koreatech.in.domain.User.UserType;
+import koreatech.in.dto.admin.user.owner.request.OwnerUpdateRequest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +18,7 @@ import lombok.ToString;
 public class Owner extends User {
     private Integer user_id;
     private String company_registration_number;
+    private String company_registration_certificate_image_url;
     private List<OwnerAttachment> attachments;
     private List<Shop> shops;
 
@@ -35,6 +39,20 @@ public class Owner extends User {
         if (owner.email != null) {
             this.email = owner.email;
         }
+    }
+
+    public boolean needToUpdate(OwnerUpdateRequest request) {
+        return !Objects.equals(this.company_registration_number,request.getCompany_registration_number())
+                || !Objects.equals(this.company_registration_certificate_image_url, request.getCompany_registration_certificate_image_url())
+                || !Objects.equals(this.grant_shop, request.getGrant_shop())
+                || !Objects.equals(this.grant_event, request.getGrant_event());
+    }
+
+    public void updateAll(OwnerUpdateRequest request) {
+        this.company_registration_number=request.getCompany_registration_number();
+        this.company_registration_certificate_image_url=request.getCompany_registration_certificate_image_url();
+        this.grant_shop=request.getGrant_shop();
+        this.grant_event=request.getGrant_event();
     }
 
     public void enrichAuthComplete() {
