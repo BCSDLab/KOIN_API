71e8bb8aa7229dcd79bb2142b2362ffdd7288176
Invidam <hansupark0208@gmail.com>
2023-04-19 21:51:46 +0900

    refactor: 롬복을 이용하도록 변경

--- src/main/java/koreatech/in/domain/Global/UpdatedAt.java
+++ src/main/java/koreatech/in/domain/Global/UpdatedAt.java
@@ -2,20 +2,14 @@ package koreatech.in.domain.Global;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor(staticName = "from")
 public class UpdatedAt {
     private static final String DATE_FORM = "yyyy-MM-dd";
     private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat(DATE_FORM);
 
     private final Date updatedAt;
-
-    public UpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public static UpdatedAt from(Date updatedAt) {
-        return new UpdatedAt(updatedAt);
-    }
 
     public String dateForm() {
         return SIMPLE_DATE_FORMAT.format(updatedAt);
