5b4b786bd3526666946b4f09a0f288644db08200
daheeParkk <qkrekgml7414@naver.com>
2023-05-01 23:40:53 +0900

    feat : is_authed를 업데이트 하는 쿼리문 작성

--- src/main/java/koreatech/in/repository/admin/AdminUserMapper.java
+++ src/main/java/koreatech/in/repository/admin/AdminUserMapper.java
@@ -16,4 +16,5 @@ public interface AdminUserMapper {
     void undeleteUserLogicallyById(@Param("id") Integer id);
     Integer getTotalCountOfUnauthenticatedOwnersByCondition(@Param("condition") NewOwnersCondition condition);
     List<Owner> getUnauthenticatedOwnersByCondition(@Param("begin") Integer begin, @Param("condition") NewOwnersCondition condition);
+    void updateOwnerAuthorById(Integer ownerId);
 }
