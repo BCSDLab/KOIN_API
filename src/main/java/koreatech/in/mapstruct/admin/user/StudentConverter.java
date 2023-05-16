a5003491225a872a5278bae3797a21b27b43d70d
tmdgus1715 <songseunghyeon@songseunghyeon-ui-MacBookPro.local>
2023-05-16 09:02:57 +0900

    feat: @Mappings 추가

--- src/main/java/koreatech/in/mapstruct/admin/user/StudentConverter.java
+++ src/main/java/koreatech/in/mapstruct/admin/user/StudentConverter.java
@@ -5,6 +5,8 @@ import koreatech.in.dto.admin.user.student.request.StudentUpdateRequest;
 import koreatech.in.dto.admin.user.student.response.StudentResponse;
 import koreatech.in.dto.admin.user.student.response.StudentUpdateResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -15,5 +17,11 @@ public interface StudentConverter {
 
     Student toStudent(StudentUpdateRequest student);
 
+    @Mappings({
+            @Mapping(source = "phone_number", target = "phoneNumber"),
+            @Mapping(source = "student_number", target = "studentNumber"),
+            @Mapping(source = "identity", target = "identity"),
+            @Mapping(source = "is_graduated", target = "isGraduated")
+    })
     StudentUpdateResponse toStudentUpdateResponse(Student student);
 }
