47b8335c19bb78e44d7705646cbb973afa303e8b
JunYoung Jang <police0023@naver.com>
2023-05-18 01:03:40 +0900

    refactor:Owner 에서 User.update적용

--- src/main/java/koreatech/in/domain/User/owner/Owner.java
+++ src/main/java/koreatech/in/domain/User/owner/Owner.java
@@ -1,12 +1,10 @@
 package koreatech.in.domain.User.owner;
 
 import java.util.List;
-import java.util.Objects;
 
 import koreatech.in.domain.Shop.Shop;
 import koreatech.in.domain.User.User;
 import koreatech.in.domain.User.UserType;
-import koreatech.in.dto.admin.user.owner.request.OwnerUpdateRequest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -39,28 +37,15 @@ public class Owner extends User {
         if (owner.email != null) {
             this.email = owner.email;
         }
-    }
-
-    public boolean needToUpdate(Owner owner) {
-        return !Objects.equals(this.company_registration_number, owner.getCompany_registration_number())
-                || !Objects.equals(this.grant_shop, owner.getGrant_shop())
-                || !Objects.equals(this.grant_event, owner.getGrant_event())
-                || !Objects.equals(this.nickname, owner.getNickname())
-                || !Objects.equals(this.phone_number, owner.getPhone_number())
-                || !Objects.equals(this.name, owner.getName())
-                || !Objects.equals(this.email, owner.getEmail())
-                || !Objects.equals(this.gender, owner.getGender());
-    }
-
-    public void updateAll(Owner owner) {
-        this.company_registration_number = owner.getCompany_registration_number();
-        this.grant_shop = owner.getGrant_shop();
-        this.grant_event = owner.getGrant_event();
-        this.nickname = owner.getNickname();
-        this.phone_number = owner.getPhone_number();
-        this.name = owner.getName();
-        this.email = owner.getEmail();
-        this.gender = owner.getGender();
+        if(owner.company_registration_number!=null){
+            this.company_registration_number=owner.company_registration_number;
+        }
+        if(owner.grant_shop!=null){
+            this.grant_shop=owner.grant_shop;
+        }
+        if(owner.grant_event!=null){
+            this.grant_event=owner.grant_event;
+        }
     }
 
     public void enrichAuthComplete() {
