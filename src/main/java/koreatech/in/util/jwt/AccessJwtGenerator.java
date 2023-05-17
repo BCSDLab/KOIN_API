96756b8301a277d66443df46636709557030d6e6
Invidam <hansupark0208@gmail.com>
2023-05-17 22:49:01 +0900

    feat: AuthTemporary 도입

--- src/main/java/koreatech/in/util/jwt/AccessJwtGenerator.java
+++ src/main/java/koreatech/in/util/jwt/AccessJwtGenerator.java
@@ -4,11 +4,25 @@ import javax.crypto.SecretKey;
 import org.springframework.stereotype.Component;
 
 @Component
-public abstract class AccessJwtGenerator<T> extends AbstractJwtGenerator<T> {
+public class AccessJwtGenerator<T> extends AbstractJwtGenerator<T> {
+
+    @Override
+    protected long getTokenValidHour() {
+        throw new RuntimeException("추상 클래스의 메서드를 호출함.");
+    }
 
     @Override
     protected final SecretKey getKey() {
         return super.jwtKeyManager.getAccessKey();
     }
 
+    @Override
+    protected T toData(String subject) throws IllegalStateException {
+        throw new RuntimeException("추상 클래스의 메서드를 호출함.");
+    }
+
+    @Override
+    public String generateToken(T data) {
+        throw new RuntimeException("추상 클래스의 메서드를 호출함.");
+    }
 }
