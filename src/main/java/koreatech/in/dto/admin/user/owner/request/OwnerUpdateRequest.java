07c4c120a60827c45db3cb38ad3fc872201b92ae
JunYoung Jang <police0023@naver.com>
2023-05-18 01:04:11 +0900

    refactor:UserUpdateRequest 사용

--- src/main/java/koreatech/in/dto/admin/user/owner/request/OwnerUpdateRequest.java
+++ src/main/java/koreatech/in/dto/admin/user/owner/request/OwnerUpdateRequest.java
@@ -1,18 +1,21 @@
 package koreatech.in.dto.admin.user.owner.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import koreatech.in.dto.admin.user.request.UserUpdateRequest;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @ApiModel("AdminOwnerUpdateRequest")
-public class OwnerUpdateRequest {
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class OwnerUpdateRequest extends UserUpdateRequest {
     @NotBlank(message = "사업자 등록 번호는 필수입니다.")
     @Pattern(regexp = "^[0-9]{3}-[0-9]{2}-[0-9]{5}", message = "사업자 등록 번호 형식이 올바르지 않습니다.")
     @ApiModelProperty(notes = "사업자 등록 번호 \n"
@@ -29,23 +32,6 @@ public class OwnerUpdateRequest {
     @ApiModelProperty(notes = "이벤트수정권한", example = "false")
     private Boolean grant_event;
 
-    @Size(max = 50, message = "닉네임의 길이는 최대 50자 입니다")
-    @ApiModelProperty(notes = "사장님닉네임", example = "사장님 닉네임")
-    private String nickname;
-
-    @Pattern(regexp = "^[0-9]{3}-[0-9]{3,4}-[0-9]{4}", message = "전화번호 형식이 올바르지 않습니다.")
-    @Size(max = 20, message = "휴대전화의 길이는 최대 20자 입니다")
-    @ApiModelProperty(notes = "사장님휴대전화", example = "010-0000-0000")
-    private String phone_number;
-
-    @Size(max = 50, message = "이름의 길이는 최대 50자 입니다")
-    @ApiModelProperty(notes = "사장님성함", example = "장준영")
-    private String name;
-
-    @Size(max = 100, message = "이메일의 길이는 최대 100자입니다")
-    @ApiModelProperty(notes = "이메일 \n" + "-최대 100자", example = "testmailnotvalid@gmail.com")
-    private String email;
-
-    @ApiModelProperty(notes = "사장님성별")
-    private Integer gender;
+    @ApiModelProperty(hidden = true)
+    private String password;
 }
