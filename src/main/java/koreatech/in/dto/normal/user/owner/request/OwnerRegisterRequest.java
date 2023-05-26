422cc6f4bb46d4bf04cca046d36ab0617d8026c5
tmdgus1715 <songseunghyeon@songseunghyeon-ui-MacBookPro.local>
2023-05-27 01:33:58 +0900

    refactor: 사업자 등록 데이터 관련 swagger 정보 변경

--- src/main/java/koreatech/in/dto/normal/user/owner/request/OwnerRegisterRequest.java
+++ src/main/java/koreatech/in/dto/normal/user/owner/request/OwnerRegisterRequest.java
@@ -30,24 +30,22 @@ public class OwnerRegisterRequest extends UserRegisterRequest {
     )
     private String name;
 
-    @NotBlank(message = "사업자 등록 번호는 필수입니다.")
     @Pattern(regexp = "^[0-9]{3}-[0-9]{2}-[0-9]{5}", message = "사업자 등록 번호 형식이 올바르지 않습니다.")
     @ApiModelProperty(notes = "사업자 등록 번호 \n"
             + "- not blank \n"
             + "- 사업자 등록 번호 형식이어야 함"
             , example = "012-34-56789"
-            , required = true
+            , required = false
     )
     private String companyNumber;
 
-    @NotNull(message = "이미지 첨부는 필수입니다.")
     @Size(min = 3, max = 5, message = "이미지는 사업자등록증, 영업신고증, 통장사본을 포함하여 최소 3개 최대 5개까지 가능합니다.")
     @ApiModelProperty(notes = "첨부 이미지들 \n"
             + "- not null \n"
             + "- 이미지는 최소 3개 최대 5개까지 허용됨 \n"
             + "- 모든 이미지들이 코인 이미지 형식이어야 함 \n"
             , dataType = "[Lkoreatech.in.dto.global.AttachmentUrlRequest;"
-            , required = true
+            , required = false
     )
     @Valid
     private List<AttachmentUrlRequest> attachmentUrls;
