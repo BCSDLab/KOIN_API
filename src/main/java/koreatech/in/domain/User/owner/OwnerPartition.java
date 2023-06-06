b03cbdd682248d58e9f2c34f6924efd24892e7f7
tmdgus1715 <songseunghyeon@songseunghyeon-ui-MacBookPro.local>
2023-06-06 15:27:30 +0900

    refactor: OwnerPartition 메소드 사용 제한 추가

--- src/main/java/koreatech/in/domain/User/owner/OwnerPartition.java
+++ src/main/java/koreatech/in/domain/User/owner/OwnerPartition.java
@@ -1,48 +1,16 @@
 package koreatech.in.domain.User.owner;
 
-import koreatech.in.domain.Shop.Shop;
-
 import java.util.List;
 
 public class OwnerPartition extends Owner {
     @Override
-    public Integer getUser_id() {
-        return super.getUser_id();
+    public void setCompany_registration_number(String company_registration_number) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
-    public void setUser_id(Integer user_id) {
-        super.setUser_id(user_id);
-    }
-
-    @Override
-    public Boolean getGrant_shop() {
-        return super.getGrant_shop();
-    }
-
-    @Override
-    public Boolean getGrant_event() {
-        return super.getGrant_event();
-    }
-
-    @Override
-    public List<Shop> getShops() {
-        return super.getShops();
-    }
-
-    @Override
-    public void setShops(List<Shop> shops) {
-        super.setShops(shops);
-    }
-
-    @Override
-    public void setGrant_shop(Boolean grant_shop) {
-        super.setGrant_shop(grant_shop);
-    }
-
-    @Override
-    public void setGrant_event(Boolean grant_event) {
-        super.setGrant_event(grant_event);
+    public void setAttachments(List<OwnerAttachment> attachments) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
