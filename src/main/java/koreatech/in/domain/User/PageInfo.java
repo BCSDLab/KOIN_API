69146ffa21be533c8c93a5d48884e4c34e0de96f
tmdgus1715 <songseunghyeon@songseunghyeon-ui-MacBookPro.local>
2023-06-15 09:28:28 +0900

    feat: 페이지 정보 나타내는 도메인 PageInfo정의

---
+++ src/main/java/koreatech/in/domain/User/PageInfo.java
@@ -0,0 +1,35 @@
+package koreatech.in.domain.User;
+
+import koreatech.in.exception.BaseException;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import static koreatech.in.exception.ExceptionInformation.PAGE_NOT_FOUND;
+
+@Getter
+public class PageInfo {
+    Integer totalPage;
+    Integer totalCount;
+    Integer currentPage;
+    Integer limit;
+
+    @Builder
+    private PageInfo(Integer totalCount, Integer currentPage, Integer limit) {
+        this.totalCount = totalCount;
+        this.currentPage = currentPage;
+        this.limit = limit;
+        this.totalPage = extractTotalPage();
+        validatePageInfo();
+    }
+
+    private Integer extractTotalPage() {
+        return totalCount.equals(0) ? 1 : (int) Math.ceil((double) totalCount / this.limit);
+    }
+
+    private void validatePageInfo() {
+        if (currentPage > totalPage) {
+            throw new BaseException(PAGE_NOT_FOUND);
+        }
+    }
+}
