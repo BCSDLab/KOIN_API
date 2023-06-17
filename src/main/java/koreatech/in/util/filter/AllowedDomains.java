1452a25873bac40ee27f40f6cec41b654c8081eb
Invidam <hansupark0208@gmail.com>
2023-06-17 17:16:09 +0900

    feat: 유지할 자료구조 변경

--- src/main/java/koreatech/in/util/filter/AllowedDomains.java
+++ src/main/java/koreatech/in/util/filter/AllowedDomains.java
@@ -1,97 +1,56 @@
 package koreatech.in.util.filter;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
 public class AllowedDomains {
+    private static final String LINE_DELIMITER = ",";
 
-    public static final String DELIMETER = ",";
-    public static final String DOT = ".";
-    public static final String PROTOCOL_PREFIX = "https://";
+    private final Set<Origin> allowedOrigins;
+    private final Set<Origin> allowedOriginsWithEmptyPort;
 
-    private final String originHost;
-
-    private final List<String> crossHosts;
-
-    /**
-     * TODO
-     * 1. https(프로토콜), port(포트번호) 처리
-     * 2. 입력 중에서 무엇을 사용할 것인지? 전체 URL을 사용해야 하는 것 아닌지
-     * 3. domain은 전체가 아니라 host(doamin)만 갖고 있음.
-     * 4. 이메일 처리
-     */
     @Autowired
-    public AllowedDomains(@Value("${project.domain}") String allowDomain,
-                          @Value("${cross.domains}") String crossDomainsLine) {
-        this.originHost = makeDomainHost(allowDomain);
-        this.crossHosts = makeCrossHosts(crossDomainsLine);
+    public AllowedDomains(@Value("${allowed.origins}") String allowDomainsLine) {
+        this.allowedOrigins = makeOrigins(allowDomainsLine);
+        this.allowedOriginsWithEmptyPort = makeOriginsWithEmptyPort(allowDomainsLine);
     }
 
-    public boolean canAllow(String requestHost) {
-        if (isEmpty(requestHost)) {
+    public boolean include(String clientUrl, String serverUrl) {
+        if(clientUrl == null || clientUrl.isEmpty()) {
             return false;
         }
-
-        return isIncludedInAllowedDomain(requestHost) || isIncludedInCrossDomains(requestHost);
-    }
-
-    private boolean isEmpty(String requestHost) {
-        return requestHost == null || requestHost.isEmpty();
-    }
-
-    private boolean isIncludedInAllowedDomain(String requestHost) {
-        return isIncludedIn(requestHost, this.originHost);
-    }
-
-    private boolean isIncludedInCrossDomains(String requestHost) {
-        Optional<String> allowedDomain = crossHosts.stream()
-                .filter(crossDomainHosts -> isIncludedIn(requestHost, crossDomainHosts)).findAny();
-
-        return allowedDomain.isPresent();
-    }
-
-    private boolean isIncludedIn(String requestHost, String allowedDomainHost) {
-        return allowedDomainHost.equals(requestHost) || requestHost.endsWith(suffixFrom(requestHost));
-    }
-
-    private Optional<String> getHost(String domain) {
-        try {
-            return Optional.of(new URL(makeUrl(domain)).getHost());
-        } catch (MalformedURLException exception) {
-            exception.printStackTrace();
-            return Optional.empty();
+        if (isSameOrigin(clientUrl, serverUrl)) {
+            return true;
         }
+
+        return isAllowedUrl(Origin.from(clientUrl));
     }
 
-    private String makeUrl(String domain) {
-        return PROTOCOL_PREFIX + domain;
+    private boolean isSameOrigin(String clientUrl, String serverUrl) {
+        return clientUrl.equals(serverUrl);
     }
 
-    private String suffixFrom(String requestHost) {
-        return DOT + requestHost;
+    private boolean isAllowedUrl(Origin clinetOrigin) {
+        return allowedOrigins.contains(clinetOrigin) || allowedOriginsWithEmptyPort.contains(clinetOrigin.withEmptyPort());
     }
 
-    private String makeDomainHost(String allowDomain) {
-        return getHost(allowDomain)
-                .orElseThrow(() -> new RuntimeException("In AllowedDomains, project.domain is invalid."));
+    private Set<Origin> makeOrigins(String allowDomainLine) {
+        return Arrays.stream(allowDomainLine.split(LINE_DELIMITER))
+                .map(Origin::from)
+                .filter(origin -> !origin.isPortEmpty())
+                .collect(Collectors.toSet());
     }
 
-    private List<String> makeCrossHosts(String crossDomainsLine) {
-        return Arrays.stream(crossDomainsLine.split(DELIMETER))
-                .map(crossDomain -> getHost(crossDomain).orElseThrow(
-                        () -> new RuntimeException("In AllowedDomains, cross.doamins are invalid.")))
-                .collect(Collectors.toList());
+    private Set<Origin> makeOriginsWithEmptyPort(String allowDomainLine) {
+        return Arrays.stream(allowDomainLine.split(LINE_DELIMITER))
+                .map(Origin::from)
+                .filter(Origin::isPortEmpty)
+                .collect(Collectors.toSet());
     }
 
 }
---
+++ src/main/java/koreatech/in/util/filter/Origin.java
@@ -0,0 +1,68 @@
+package koreatech.in.util.filter;
+
+import java.net.URI;
+import java.util.Objects;
+
+public class Origin {
+    private final URI uri;
+
+    private Origin(URI uri) {
+        this.uri = uri;
+    }
+
+    public static Origin from(String url) {
+        return new Origin(makeURI(url));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Origin other = (Origin) o;
+
+        return isEqualScheme(other) && isEqualHost(other) && isEqualPort(other);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uri.getScheme(), uri.getHost(), uri.getPort());
+    }
+
+    private boolean isEqualPort(Origin other) {
+        return this.uri.getPort() == other.uri.getPort();
+    }
+
+    public boolean isPortEmpty() {
+        return this.uri.getPort() == -1;
+    }
+
+//    private boolean isSubDomain(URI clinetURI) {
+//        return clinetURI.getHost().endsWith(this.uri.getHost());
+//    }
+//
+    private boolean isEqualHost(Origin other) {
+        return this.uri.getHost().equals(other.uri.getHost());
+    }
+
+    private boolean isEqualScheme(Origin other) {
+        return this.uri.getScheme().equals(other.uri.getScheme());
+    }
+
+    private static URI makeURI(String url) {
+        try {
+            return URI.create(url);
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException(String.format("allowed.origins의 %s가 표준에 맞지 않습니다.", url));
+        } catch (NullPointerException exception) {
+            throw new IllegalArgumentException(String.format("allowed.origins의 %s가 비어있습니다.", url));
+        }
+    }
+
+    public Origin withEmptyPort() {
+        return Origin.from(this.uri.getScheme() + "://" + this.uri.getHost());
+    }
+}
