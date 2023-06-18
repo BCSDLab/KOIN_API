5e7a404666cbe3bde7db45d2e4c5613c62a6d8a5
Invidam <hansupark0208@gmail.com>
2023-06-19 00:40:54 +0900

    feat: 어드민 메뉴 카테고리 수정 API 추가

--- src/main/java/koreatech/in/mapstruct/admin/shop/AdminShopMenuConverter.java
+++ src/main/java/koreatech/in/mapstruct/admin/shop/AdminShopMenuConverter.java
@@ -1,8 +1,10 @@
 package koreatech.in.mapstruct.admin.shop;
 
-import koreatech.in.domain.Shop.ShopMenuProfile;
 import koreatech.in.domain.Shop.ShopMenu;
+import koreatech.in.domain.Shop.ShopMenuCategory;
+import koreatech.in.domain.Shop.ShopMenuProfile;
 import koreatech.in.dto.admin.shop.request.CreateShopMenuRequest;
+import koreatech.in.dto.admin.shop.request.UpdateShopMenuCategoryRequest;
 import koreatech.in.dto.admin.shop.response.AllMenusOfShopResponse;
 import koreatech.in.dto.admin.shop.response.ShopMenuResponse;
 import org.mapstruct.Mapper;
@@ -23,4 +25,7 @@ public interface AdminShopMenuConverter {
     ShopMenuResponse toMenuResponse(ShopMenuProfile shopMenuProfile);
 
     AllMenusOfShopResponse.Menu toAllMenusOfShopResponse$Menu(ShopMenuProfile shopMenuProfile);
+
+    @Mapping(source = "shopId", target="shop_id")
+    ShopMenuCategory toMenuCategory(Integer shopId, UpdateShopMenuCategoryRequest updateShopMenuCategoryRequest);
 }
