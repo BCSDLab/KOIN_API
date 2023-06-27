ea2a0d3f733e3dcb1544ceaf8f2c7344433fd265
tmdgus1715 <songseunghyeon@songseunghyeon-ui-MacBookPro.local>
2023-06-27 20:57:24 +0900

    feat: 사장님정보와 요청 상점 정보 가진 도메인 클래스 정의

---
+++ src/main/java/koreatech/in/domain/User/owner/OwnerIncludingShop.java
@@ -0,0 +1,17 @@
+package koreatech.in.domain.User.owner;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OwnerIncludingShop extends Owner {
+    Integer shop_id;
+    String shop_name;
+}
