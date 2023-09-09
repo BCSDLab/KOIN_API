8cf2e23166e211985acae8eac7b4232302f9ea4e
Invidam <hansupark0208@gmail.com>
2023-09-09 22:31:37 +0900

    refactor: JwtManager 대신 JwtUtil을 접근하도록 변경

--- src/main/java/koreatech/in/service/JwtValidator.java
+++ src/main/java/koreatech/in/service/JwtValidator.java
@@ -1,27 +1,36 @@
 package koreatech.in.service;
 
+import static koreatech.in.exception.ExceptionInformation.BAD_ACCESS;
+import static koreatech.in.exception.ExceptionInformation.TOKEN_EXPIRED;
+
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
 import koreatech.in.domain.User.User;
+import koreatech.in.exception.BaseException;
 import koreatech.in.repository.user.UserMapper;
-import koreatech.in.util.jwt.TemporaryAccessJwtManager;
-import koreatech.in.util.jwt.UserAccessJwtManager;
+import koreatech.in.util.jwt.JwtKeyManager;
+import koreatech.in.util.jwt.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-
+//todo AccessJwtValidator, RefreshJwtValidator로 분리
 @Service
 public class JwtValidator {
     private static final String AUTH_PREFIX = "Bearer ";
     public static final String AUTHORIZATION = "Authorization";
 
     @Autowired
-    private UserAccessJwtManager userAccessJwtGenerator;
-    @Autowired
-    private TemporaryAccessJwtManager temporaryAccessJwtGenerator;
+    protected JwtKeyManager jwtKeyManager;
+
     @Autowired
     private UserMapper userMapper;
+
+    private final JwtUtil jwtUtil = new JwtUtil();
 
     public User validate() {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
@@ -39,11 +48,14 @@ public class JwtValidator {
         return userMapper.getAuthedUserById(userId);
     }
 
-    public boolean isValidTemporaryAccessTokenInHeader(String header) {
-        if(!isAuthHeader(header)) {
-            return false;
-        }
-        return !temporaryAccessJwtGenerator.isExpired(makeTokenFromHeader(header));
+    public void validateTemporaryAccessTokenInHeader(String header) {
+        validateToken(makeTokenFromHeader(header), getKey());
+    }
+
+    public Integer validateAndGetUserId() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        return getUserId(request.getHeader(AUTHORIZATION));
     }
 
     private Integer getUserId(String header) {
@@ -55,8 +67,8 @@ public class JwtValidator {
         if (accessToken.equals("undefined")) { // 추후 프론트엔드 측에서 변경
             return null;
         }
-
-        return userAccessJwtGenerator.getDataFromToken(accessToken);
+        String subject = jwtUtil.getSubject(accessToken, getKey());
+        return Integer.parseInt(subject);
     }
 
     private boolean isAuthHeader(String header) {
@@ -67,9 +79,19 @@ public class JwtValidator {
         return header.substring(AUTH_PREFIX.length());
     }
 
-    public Integer validateAndGetUserId() {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-
-        return getUserId(request.getHeader(AUTHORIZATION));
+    private void validateToken(String token, SecretKey key) {
+        try {
+            if (jwtUtil.isExpiredToken(token, key)) {
+                throw new BaseException(TOKEN_EXPIRED);
+            }
+        } catch (UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e) {
+            //보안상 목적으로 상세하게 표현하지 않는다.
+            throw new BaseException(BAD_ACCESS);
+        }
     }
+
+    private SecretKey getKey() {
+        return jwtKeyManager.getAccessKey();
+    }
+
 }
