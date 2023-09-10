c99c607e96b3666ca7560a92fc8537d5f10a86f3
Invidam <hansupark0208@gmail.com>
2023-09-10 14:23:42 +0900

    refactor: JwtValidator도 AccessJwtValidaotr을 활용하도록 변경

--- src/main/java/koreatech/in/service/JwtValidator.java
+++ src/main/java/koreatech/in/service/JwtValidator.java
@@ -6,13 +6,10 @@ import static koreatech.in.exception.ExceptionInformation.TOKEN_EXPIRED;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.SignatureException;
-import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
 import koreatech.in.domain.User.User;
 import koreatech.in.exception.BaseException;
 import koreatech.in.repository.user.UserMapper;
-import koreatech.in.util.jwt.JwtKeyManager;
-import koreatech.in.util.jwt.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -24,12 +21,10 @@ public class JwtValidator {
     public static final String AUTHORIZATION = "Authorization";
 
     @Autowired
-    protected JwtKeyManager jwtKeyManager;
+    private AccessJwtValidator accessJwtValidator;
 
     @Autowired
     private UserMapper userMapper;
-
-    private final JwtUtil jwtUtil = new JwtUtil();
 
     public User validate() {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
@@ -54,7 +49,7 @@ public class JwtValidator {
     }
 
     public void validateTemporaryAccessTokenInHeader(String header) {
-        validateToken(makeTokenFromHeader(header), getKey());
+        validateToken(makeTokenFromHeader(header));
     }
 
     private Integer getUserId(String header) {
@@ -66,8 +61,8 @@ public class JwtValidator {
         if (accessToken.equals("undefined")) { // 추후 프론트엔드 측에서 변경
             return null;
         }
-        String subject = jwtUtil.getSubject(accessToken, getKey());
-        return Integer.parseInt(subject);
+
+        return accessJwtValidator.getUserIdInToken(accessToken);
     }
 
     private boolean isAuthHeader(String header) {
@@ -78,19 +73,15 @@ public class JwtValidator {
         return header.substring(AUTH_PREFIX.length());
     }
 
-    private void validateToken(String token, SecretKey key) {
+    private void validateToken(String token) {
         try {
-            if (jwtUtil.isExpiredToken(token, key)) {
+            if (accessJwtValidator.isExpiredToken(token)) {
                 throw new BaseException(TOKEN_EXPIRED);
             }
         } catch (UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e) {
             //보안상 목적으로 상세하게 표현하지 않는다.
             throw new BaseException(BAD_ACCESS);
         }
-    }
-
-    private SecretKey getKey() {
-        return jwtKeyManager.getAccessKey();
     }
 
 }
