cb69b3a372964e454e41938238717ce506c2f05f
Invidam <hansupark0208@gmail.com>
2023-10-01 18:29:20 +0900

    feat: 검증 강화

--- src/main/java/koreatech/in/argumentresolver/UserArgumentResolver.java
+++ src/main/java/koreatech/in/argumentresolver/UserArgumentResolver.java
@@ -1,23 +1,31 @@
 package koreatech.in.argumentresolver;
 
+import koreatech.in.annotation.Auth;
 import koreatech.in.annotation.Login;
-import koreatech.in.service.AccessJwtValidator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-
+/*
+* @Auth-AuthInterceptor을 통해 검증이 이루어진 API 라면,
+* @Login User user과 같은 형식으로 사용자 정보를 받아올 수 있다.
+* */
 @Component
 public class UserArgumentResolver implements HandlerMethodArgumentResolver {
-    @Autowired
-    private AccessJwtValidator accessJwtValidator;
-
     @Override
     public boolean supportsParameter(MethodParameter methodParameter) {
+        return hasLoginAnnotation(methodParameter) && isAuthVerified(methodParameter);
+    }
+
+    private boolean hasLoginAnnotation(MethodParameter methodParameter) {
         return methodParameter.hasParameterAnnotation(Login.class);
+    }
+
+    private boolean isAuthVerified(MethodParameter methodParameter) {
+        return methodParameter.getDeclaringClass().isAnnotationPresent(Auth.class) || methodParameter.getMethod()
+                .isAnnotationPresent(Auth.class);
     }
 
     @Override
